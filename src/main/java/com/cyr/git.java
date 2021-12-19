27cff490618811645acd4e9ee6e3c60fcdef5c5b
975131701 <975131701@qq.com>
2021-12-19 18:46:25 +0800

    master-conflict commit

--- src/main/java/com/cyr/git.java
+++ src/main/java/com/cyr/git.java
@@ -9,5 +9,6 @@ public class git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git hot-fix");
+        System.out.println("hello git master");
     }
 }
