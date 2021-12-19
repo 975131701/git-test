56a6df729f8c2b30ae5fc16d92d43d9858cbc283
975131701 <975131701@qq.com>
2021-12-19 18:39:41 +0800

    first commit

---
+++ src/main/java/com/cyr/git.java
@@ -0,0 +1,12 @@
+package com.cyr;
+
+/**
+ * @author cyr
+ * @version 1.0
+ * @date 2021/12/19 18:37
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
